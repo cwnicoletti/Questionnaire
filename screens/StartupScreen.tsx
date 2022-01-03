cc6032d968d4fd2e90d00e53c6513b6570274209
christiannicoletti <cwnicoletti@gmail.com>
2022-01-03 07:51:52 -0800

    change: MainNavigator and StartupScreen

--- screens/StartupScreen.tsx
+++ screens/StartupScreen.tsx
@@ -1,76 +1,88 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import React, { useEffect } from "react";
-import { ActivityIndicator, StyleSheet, View } from "react-native";
-import { useAppDispatch } from "../hooks";
-import { authenticate, logout } from "../store/actions/auth/auth";
-import { getUserData } from "../store/actions/user/user";
+import React, { useCallback, useEffect, useState, useRef } from "react";
+import { Image, StyleSheet, Animated } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { Asset } from "expo-asset";
+import AppLoading from "expo-app-loading";
 
 const StartupScreen = (props) => {
-  const dispatch = useAppDispatch();
+  const [appIsReady, setAppIsReady] = useState(false);
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+
+  const prepare = async () => {
+    try {
+      // Keep the splash screen visible while we fetch resources
+      await SplashScreen.preventAutoHideAsync();
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      // Tell the application to render
+      setAppIsReady(true);
+    }
+  };
+
+  const tryLogin = async () => {
+    props.navigation.navigate("StartAuth");
+  };
+
   useEffect(() => {
-    const tryLogin = async () => {
-      const userData = await AsyncStorage.getItem("userLoginData");
-      const introingData = await AsyncStorage.getItem("introing");
-      const permissionsData = await AsyncStorage.getItem("gettingPermissions");
-
-      const transformedIntroing = JSON.parse(introingData);
-      const transformedPermissions = JSON.parse(permissionsData);
-      const transformedData = JSON.parse(userData);
-
-      let [localId, token, gettingPermissions, introing] = ["", "", true, true];
-      if (transformedPermissions) {
-        [{ gettingPermissions }] = [transformedPermissions];
-      }
-
-      if (transformedIntroing) {
-        [{ introing }] = [transformedIntroing];
-      }
-
-      if (transformedData) {
-        [{ localId, token }] = [transformedData];
-      }
-
-      if (!transformedData && !introing && !gettingPermissions) {
-        props.navigation.navigate("StartAuth");
-        return;
-      }
-
-      if ((!token || !localId) && !introing) {
-        props.navigation.navigate("StartAuth");
-        return;
-      }
-      
-      if (introing) {
-        props.navigation.navigate("MainNavigator");
-        return;
-      }
-
-      if (gettingPermissions) {
-        await dispatch(getUserData());
-        await dispatch(authenticate(localId, token));
-        props.navigation.navigate("PermissionsStack");
-      } else {
-        await dispatch(getUserData());
-        await dispatch(authenticate(localId, token));
-        await props.navigation.navigate("Exhibit");
-      }
-    };
-
+    prepare();
     tryLogin();
   }, []);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  const cacheResourcesAsync = async () => {
+    const images = [
+      require("../assets/ExhibitU_icon_transparent_white_spaced.png"),
+    ];
+
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+    return Promise.all(cacheImages);
+  };
+
+  if (!appIsReady) {
+    return (
+      <AppLoading
+        startAsync={async () => {
+          await cacheResourcesAsync();
+        }}
+        onFinish={async () => {
+          await onLayoutRootView();
+        }}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
-    <View style={styles.screen}>
-      <ActivityIndicator size="large" color="white" />
-    </View>
+    <Animated.View style={{ ...styles.screen, opacity: fadeAnim }}>
+      <Image
+        source={require("../assets/ExhibitU_icon_transparent_white_spaced.png")}
+        style={{ height: "100%", width: "100%" }}
+        resizeMode="contain"
+      />
+    </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1,
+    height: "100%",
+    width: "100%",
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#000000",
   },
 });
 
