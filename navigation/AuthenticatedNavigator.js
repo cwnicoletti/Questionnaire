3bcc13e193097855ec53b1c990a3eecc7e818ca1
christiannicoletti <cwnicoletti@gmail.com>
2022-01-05 18:43:32 -0800

    add: screens to AuthenticatedNavigator

--- navigation/AuthenticatedNavigator.js
+++ navigation/AuthenticatedNavigator.js
@@ -1,30 +1,61 @@
 import React from "react";
-import {
-  createStackNavigator,
-} from "@react-navigation/stack";
-import { TransitionPresets } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
+import { TransitionPresets } from "@react-navigation/stack";
 import SignupOrLoginScreen from "../screens/auth/SignupOrLoginScreen";
+import SignupScreen1 from "../screens/auth/SignupPhone/SignupScreen1";
+import SignupScreen2 from "../screens/auth/SignupPhone/SignupScreen2";
+import AIntroduction from "../screens/auth/CreateUser/AIntroduction";
 import StartupScreen from "../screens/StartupScreen";
 
 const Stack = createStackNavigator();
 
+const forFade = ({ current }) => ({
+  cardStyle: {
+    opacity: current.progress,
+  },
+});
+
 function AuthenticatedNavigator() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerMode: "none",
-        headerShown: false,
-        navigationOptions: {
-          headerVisible: false,
-        },
-      }}
-    >
-      <Stack.Screen name="Startup" component={StartupScreen} options={{}} />
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Startup"
+        component={StartupScreen}
+        options={{
+          gestureEnabled: false,
+          headerShown: false,
+        }}
+      />
       <Stack.Screen
         name="StartAuth"
         component={SignupOrLoginScreen}
         options={{
+          gestureEnabled: false,
+          cardStyleInterpolator: forFade,
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="AIntroduction"
+        component={AIntroduction}
+        options={{
           ...TransitionPresets.ModalSlideFromBottomIOS,
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="SignupPhoneScreen1"
+        component={SignupScreen1}
+        options={{
+          ...TransitionPresets.ModalSlideFromBottomIOS,
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="SignupPhoneScreen2"
+        component={SignupScreen2}
+        options={{
+          headerShown: false,
         }}
       />
     </Stack.Navigator>
